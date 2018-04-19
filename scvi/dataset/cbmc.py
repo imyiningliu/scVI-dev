1c908b9bfb80857ab737f3cc6f96afef570b7046
Yining Liu <lyining@berkeley.edu>
2018-04-19 11:33:39 -0700

    generate expression testing and training files

--- scvi/dataset/cbmc.py
+++ scvi/dataset/cbmc.py
@@ -18,13 +18,15 @@ class CbmcDataset(GeneExpressionDataset):
         self.download_name = 'expression.bin'
         self.data_filename = 'expression_%s.npy' % type
         self.labels_filename = 'labels_%s.npy' % type
+        self.gene_names = 'genes_names.npy'
         self.download_and_preprocess()
         super(CbmcDataset, self).__init__([np.load(self.save_path + self.data_filename)],
                                             [np.load(self.save_path + self.labels_filename)])
 
     def download(self):
-        # Generating samples according to a ZINB process
         url = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE100866&format=file&file=GSE100866%5FCBMC%5F8K%5F13AB%5F10X%2DRNA%5Fumi%2Ecsv%2Egz"
+        print("Downloading CBMC data")
+
         # Create the path to save the data
         if not os.path.exists(self.save_path):
             os.makedirs(self.save_path)
@@ -33,34 +35,26 @@ class CbmcDataset(GeneExpressionDataset):
             data = response.read()  # a `bytes` object
             out_file.write(data)
 
-# TODO: Unzip the file
-#        f = gzip.open(self.save_path + expression, 'rb')
-#        file_content = f.read()
-#        f.close()
-
     def preprocess(self):
         print("Preprocessing CBMC data")
-        expression = pd.read_csv(self.save_path + filename, index_col=0).T
-        gene_names = expression.columns
-        selected = np.std(expression.as_matrix(), axis=0).argsort()[-600:][::-1]
-        expression = expression.as_matrix()[:, selected]
 
+        # TODO: Get labels
 
-        expression_train, expression_test, c_train, c_test, n_train, n_test = train_test_split(expression, \
-                                                                                               protein.as_matrix(), \
-                                                                                               norm_protein.as_matrix(),
-                                                                                               random_state=0)
+        expression_data = pd.read_csv(self.save_path + filename, index_col=0, compression='gzip').T
+        gene_names = expression_data.columns
+
+        selected = np.std(expression_data.as_matrix(), axis=0).argsort()[-600:][::-1]
+        expression_data = expression_data.as_matrix()[:, selected]
+        gene_names = gene_names[selected]
 
         # train test split for log-likelihood scores
-        expression_train, expression_test, c_train, c_test = GeneExpressionDataset.train_test_split(expression_data,
-                                                                                                    labels)
+        expression_train, expression_test = GeneExpressionDataset.train_test_split(expression_data)
 
-        np.savetxt(self.save_path + "expression_train", expression_train)
-        np.savetxt(self.save_path + "expression_test", expression_test)
-        np.savetxt(self.save_path + "n_train", n_train)
-        np.savetxt(self.save_path + "n_test", n_test)
-        np.savetxt(self.save_path + "c_train", c_train)
-        np.savetxt(self.save_path + "c_test", c_test)
+        np.save(self.save_path + "expression_train.npy", expression_train)
+        np.save(self.save_path + "expression_test.npy", expression_test)
+        # np.save(self.save_path + 'labels_train.npy', c_train)
+        # np.save(self.save_path + 'labels_test.npy', c_test)
+        np.save(self.save_path + self.gene_names, gene_names)
 
     def download_and_preprocess(self):
         if not (os.path.exists(self.save_path + self.data_filename) and
