4c476b9b8421222394d3e3da724903a0c6fb5496
Yining Liu <lyining@berkeley.edu>
2018-04-19 23:37:43 -0700

    change import statement back

--- scvi/dataset/__init__.py
+++ scvi/dataset/__init__.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from scvi.dataset.brain_large import BrainLargeDataset
-from scvi.dataset.cortex import CortexDataset
-from scvi.dataset.dataset import GeneExpressionDataset
-from scvi.dataset.synthetic import SyntheticDataset
-from scvi.dataset.cbmc import CbmcDataset
+from .brain_large import BrainLargeDataset
+from .cortex import CortexDataset
+from .dataset import GeneExpressionDataset
+from .synthetic import SyntheticDataset
+from .cbmc import CbmcDataset
 
 __all__ = ['SyntheticDataset',
            'CortexDataset',
